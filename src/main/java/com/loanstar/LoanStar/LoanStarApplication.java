841b32fdd003bbe3ce2cdb7041e18ea8c2552988
Liuvis Naranjo <lnaranj5@asu.edu>
2022-10-14 13:05:02 -0400

    initial commit

---
+++ src/main/java/com/loanstar/LoanStar/LoanStarApplication.java
@@ -0,0 +1,13 @@
+package com.loanstar.LoanStar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanStarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loanstar/LoanStar/LoanStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanstar.LoanStar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
